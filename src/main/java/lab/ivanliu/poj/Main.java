cc6079b08afd7fce4905d53b607128eeec9b93ef
Mr Ivan <ivanliu1218@hotmail.com>
2018-07-22 21:25:07 +0800

    POJ-1852: accepted

--- src/main/java/lab/ivanliu/poj/Main.java
+++ src/main/java/lab/ivanliu/poj/Main.java
@@ -8,19 +8,40 @@ public class Main {
     private static Main solution = new Main();
 
     public static void main(String[] args) throws Exception {
-        solution.poj1000();
+        solution.poj1852();
     }
 
-    public void poj1000() {
+    private int[] minTime;
+    private int[] maxTime;
+    public void poj1852() {
         Scanner cin = new Scanner(System.in);
-        int a = cin.nextInt();
-        int b = cin.nextInt();
-        int c = poj1000(a, b);
-        System.out.println(c);
+        int cases = cin.nextInt();
+        minTime = new int[cases];
+        maxTime = new int[cases];
+        for (int c = 0; c < cases; ++c) {
+            int L = cin.nextInt();
+            int n = cin.nextInt();
+            int[] ants = new int[n];
+            for (int i = 0; i < n; ++i) {
+                ants[i] = cin.nextInt();
+            }
+            poj1852(L, ants, c);
+        }
+        for (int i = 0; i < cases; ++i) {
+            System.out.println(minTime[i] + " " + maxTime[i]);
+        }
     }
-
-    public int poj1000(int a, int b) {
-        return a + b;
+    public void poj1852(int L, int[] ants, int caseNum) {
+        minTime[caseNum] = Integer.MIN_VALUE;
+        for (int i = 0; i < ants.length; ++i) {
+            int t = Math.min(ants[i], L - ants[i]);
+            if (t > minTime[caseNum]) minTime[caseNum] = t;
+        }
+        maxTime[caseNum] = Integer.MIN_VALUE;
+        for (int i = 0; i < ants.length; ++i) {
+            int t = Math.max(ants[i], L - ants[i]);
+            if (t > maxTime[caseNum]) maxTime[caseNum] = t;
+        }
     }
 
 }
--- src/main/java/lab/ivanliu/poj/Solution.java
+++ src/main/java/lab/ivanliu/poj/Solution.java
@@ -16,4 +16,41 @@ public class Solution {
         return a + b;
     }
 
+    /**
+     *  POJ: 1852
+     *  Book: 1.6.2, P18
+     */
+    private int[] minTime;
+    private int[] maxTime;
+    public void poj1852() {
+        Scanner cin = new Scanner(System.in);
+        int cases = cin.nextInt();
+        minTime = new int[cases];
+        maxTime = new int[cases];
+        for (int c = 0; c < cases; ++c) {
+            int L = cin.nextInt();
+            int n = cin.nextInt();
+            int[] ants = new int[n];
+            for (int i = 0; i < n; ++i) {
+                ants[i] = cin.nextInt();
+            }
+            poj1852(L, ants, c);
+        }
+        for (int i = 0; i < cases; ++i) {
+            System.out.println(minTime[i] + " " + maxTime[i]);
+        }
+    }
+    public void poj1852(int L, int[] ants, int caseNum) {
+        minTime[caseNum] = Integer.MIN_VALUE;
+        for (int i = 0; i < ants.length; ++i) {
+            int t = Math.min(ants[i], L - ants[i]);
+            if (t > minTime[caseNum]) minTime[caseNum] = t;
+        }
+        maxTime[caseNum] = Integer.MIN_VALUE;
+        for (int i = 0; i < ants.length; ++i) {
+            int t = Math.max(ants[i], L - ants[i]);
+            if (t > maxTime[caseNum]) maxTime[caseNum] = t;
+        }
+    }
+
 }
